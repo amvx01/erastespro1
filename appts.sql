a5e962a695ed55349a1db11666f6237e4105964f
amvx01 <152839786+amvx01@users.noreply.github.com>
2023-12-04 14:21:59 +0700

    Update appts.sql

--- appts.sql
+++ appts.sql
@@ -10,8 +10,8 @@ ticket_id SERIAL,
 );
 insert into concert_tickets (concert_name, ticket_type, price, buyer_name, email, phone_number, purchase_date)
 VALUES
-  ('Taylor Swift Eras Tour', 'VIP', 150.00, 'John Doe', 'john@example.com', '1234567890', '2023-11-01'),
-  ('Taylor Swift Eras Tour', 'General Admission', 75.00, 'Jane Smith', 'jane@example.com', '9876543210', '2023-11-02'),
-  ('Taylor Swift Eras Tour', 'VIP', 150.00, 'Bob Johnson', 'bob@example.com', '5551234567', '2023-11-03'),
-  ('Taylor Swift Eras Tour', 'General Admission', 75.00, 'Alice Brown', 'alice@example.com', '9998887777', '2023-11-04');
+  ('NGAWI MUSICAL WORLD TOUR', 'VIP', 150.00, 'John Doe', 'john@example.com', '1234567890', '2023-11-01'),
+  ('NGAWI MUSICAL WORLD TOUR', 'General Admission', 75.00, 'Jane Smith', 'jane@example.com', '9876543210', '2023-11-02'),
+  ('NGAWI MUSICAL WORLD TOUR', 'VIP', 150.00, 'Bob Johnson', 'bob@example.com', '5551234567', '2023-11-03'),
+  ('NGAWI MUSICAL WORLD TOUR', 'General Admission', 75.00, 'Alice Brown', 'alice@example.com', '9998887777', '2023-11-04');
 select *from concert_tickets ct 
